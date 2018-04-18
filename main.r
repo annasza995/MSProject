6cc4ffeb6e2d257d24ce7211868a86e1624c2b87
Mateusz Serkies <mserkies@artifexmundi.com>
2018-04-18 12:16:37 +0200

    Added measures of association enum. Refactored code.

--- main.r
+++ main.r
@@ -1,9 +1,11 @@
 blueberriesData <- scan("blueberriesData.txt", double(), quote = "")
 redberriesData <- scan("redberriesData.txt", double(), quote = "")
 
-measuresOfAssociation <- function(data)
+EMeasuresOfAssociation <- list(Mean = 1, Mode = 2, Minimum = 3, FirstQuartile = 4, Median = 5, ThirdQuartile = 6, Maximum = 7)
+
+MeasuresOfAssociation <- function(data)
 {
-  Mode <- function(data) 
+  Mode <- function(data)  
   {
     uniqueVector <- unique(data)
     countTab <- tabulate(match(data, uniqueVector))
@@ -20,26 +22,26 @@ measuresOfAssociation <- function(data)
   
   result <- vector(mode = "list", length = 7)
 
-  names(result)[1] = "Mean"
-  result[1] <- mean(data)
+  names(result)[EMeasuresOfAssociation$Mean] = "Mean"
+  result[EMeasuresOfAssociation$Mean] <- mean(data)
   
-  names(result)[2] = "Mode"
-  result[2] <- Mode(data)
+  names(result)[EMeasuresOfAssociation$Mode] = "Mode"
+  result[EMeasuresOfAssociation$Mode] <- Mode(data)
   
-  names(result)[3] ="Null quartile - minumum"
-  result[3] <- quantile(data, 0.00)
+  names(result)[EMeasuresOfAssociation$Minimum] ="Null quartile - minumum"
+  result[EMeasuresOfAssociation$Minimum] <- quantile(data, 0.00)
   
-  names(result)[4] ="First quartile"
-  result[4] <- quantile(data, 0.25)
+  names(result)[EMeasuresOfAssociation$FirstQuartile] ="First quartile"
+  result[EMeasuresOfAssociation$FirstQuartile] <- quantile(data, 0.25)
   
-  names(result)[5] ="Second quartile - median"
-  result[5] <- quantile(data, 0.50)
+  names(result)[EMeasuresOfAssociation$Median] ="Second quartile - median"
+  result[EMeasuresOfAssociation$Median] <- quantile(data, 0.50)
   
-  names(result)[6] ="Third quartile"
-  result[6] <- quantile(data, 0.75)
+  names(result)[EMeasuresOfAssociation$ThirdQuartile] ="Third quartile"
+  result[EMeasuresOfAssociation$ThirdQuartile] <- quantile(data, 0.75)
   
-  names(result)[7] ="Fourth quartile - maximum"
-  result[7] <- quantile(data, 1.00)
+  names(result)[EMeasuresOfAssociation$Maximum] ="Fourth quartile - maximum"
+  result[EMeasuresOfAssociation$Maximum] <- quantile(data, 1.00)
   
   return (result);
 }
